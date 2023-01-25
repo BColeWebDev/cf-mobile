71b14c0e10bae5c4dad2ba44b7479d0c02263133
BColeWebDev <Briancole1994@hotmail.com>
2023-01-24 20:07:16 -0500

    added - axios testing api

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from "@react-native-material/core";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
+
+interface Workout {
+page:number,
+pageDisplay:number,
+items:[]
+}
 export default function App() {
+const [data, setdata] = useState<Workout>();
+useEffect(() => {
+axios.get("http://localhost:8080/api/v1/workouts/exercises?page=1&limit=10").then((response) =>{
+setdata(response.data)
+
+
+})
+}, []);
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Button title="Click Me" onPress={() => alert("🎉🎉🎉")}/>
+      <Text>{data?.page}</Text>
+      <Text>{data?.pageDisplay}</Text>
+      {data?.items.map((item:any, idx) =>  <Text key={idx}>{item.name}</Text>)}
+    
     </View>
   );
 }
