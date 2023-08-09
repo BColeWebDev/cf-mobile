6698063a2777816ea37e3659739299b9a092e02e
BColeWebDev <Briancole1994@hotmail.com>
2023-08-08 23:40:05 -0400

    modal screens

---
+++ screens/Modals/WorkoutsModal/index.tsx
@@ -0,0 +1,16 @@
+import { View , } from 'react-native'
+import React from 'react'
+import { Text,Button} from '@react-native-material/core';
+import { useSelector } from 'react-redux';
+
+const WorkoutsModal = ({navigation}) => {
+    const {workouts, isLoading,equipments, bodyTargets,muscles} = useSelector((state:any) =>state.workouts)
+  return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "space-between", }}>
+          <Text style={{ fontSize: 30, display:"flex" ,backgroundColor:"green", color:"white", width:'100%', padding:10,fontWeight:"600"}}>Filters</Text>
+          <Button onPress={() => navigation.goBack()} title="Dismiss" />
+        </View>
+  )
+}
+
+export default WorkoutsModal
