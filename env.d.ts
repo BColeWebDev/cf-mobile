cb41d664f183acbf7f71ad8f55676ef3e576ea03
BColeWebDev <Briancole1994@hotmail.com>
2024-09-09 21:19:44 -0400

    update env

--- env.d.ts
+++ env.d.ts
@@ -2,4 +2,5 @@ declare module "@env" {
   export const EXPO_PUBLIC_APP_URL: string;
   export const EXPO_PUBLIC_API_KEY: string;
   export const EXPO_PUBLIC_API_HOST: string;
+  export const EXPO_PUBLIC_LOCAL_URL: string;
 }
